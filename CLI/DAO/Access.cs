b31e1d4429560d777b0a086588a15d9528aedd87
nkusla <nikola.kuslakovic1616@gmail.com>
2023-11-06 01:00:28 +0100

    Added generic DAO, Storage and Serializer classes
    - Generic DAO class has CRUD methods for core class manipulation
    - Storage and Serializer class are used to store data in CSV files
    - Created necessary interfaces for mentioned classes

---
+++ CLI/DAO/Access.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI.DAO;
+
+public interface IAccess
+{
+    int Id { get; set; }
+    void Copy(IAccess obj);
+}
