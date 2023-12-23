7b2f001753efa25ace6b728d53f8fdae343b0c31
nemanjaZ02 <nemanja.zekanovic002@gmail.com>
2023-12-23 21:57:51 +0100

    Added addSubjectWindow and EditSubjectWindow

--- GUI/View/AddSubjectWindow.xaml.cs
+++ GUI/View/AddSubjectWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using CLI.DAO;
+using CLI.Model;
+using GUI.DTO;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,10 +25,87 @@ namespace GUI
     public partial class AddSubjectWindow : Window
     {
         private HeadDAO _headDAO;
+        private Brush _defaultBrushBorder;
+
+        public SubjectDTO subjectDTO { get; set; }
+        public ProfessorDTO professorDTO { get; set; }
 
         public AddSubjectWindow(HeadDAO headDAO)
         {
             InitializeComponent();
+            _headDAO = headDAO;
+            DataContext = this;
+            _defaultBrushBorder = textBoxName.BorderBrush.Clone();
+
+            comboBoxSemester.ItemsSource = Enum.GetValues(typeof(SemesterEnum));
+            comboBoxProfessor.ItemsSource = headDAO.daoProfessor.GetAllObjects();
+
+            professorDTO = new ProfessorDTO();
+            subjectDTO = new SubjectDTO(professorDTO);
+
+            labelError.Content = string.Empty;
+        }
+
+        private bool InputCheck()
+        {
+            bool validInput = true;
+
+            foreach (var grid in stackPanel.Children.OfType<Grid>())
+            {
+                foreach (var control in grid.Children)
+                {
+                    if (control is TextBox)
+                    {
+                        TextBox textBox = (TextBox)control;
+                        if (textBox.Text == string.Empty)
+                        {
+                            textBox.BorderBrush = Brushes.Red;
+                            textBox.BorderThickness = new Thickness(2);
+                            validInput = false;
+                        }
+                        else
+                        {
+                            textBox.BorderBrush = _defaultBrushBorder;
+                            textBox.BorderThickness = new Thickness(1);
+                        }
+                    }
+                }
+            }
+
+            return validInput;
+        }
+
+        private void ClearAllInput()
+        {
+            textBoxCode.Text = string.Empty;
+            textBoxName.Text = string.Empty;
+            comboBoxSemester.SelectedIndex = 0; 
+            textBoxYearOfStudy.Text = string.Empty;
+            comboBoxProfessor.SelectedIndex = 0;
+            textBoxEcts.Text = string.Empty;
+        }
+
+        public void AddSubject(object sender, RoutedEventArgs e)
+        {
+            if (InputCheck())
+            {
+                _headDAO.daoSubject.AddObject(subjectDTO.ToSubject());
+
+                labelError.Content = "Subject successuflly added!";
+                labelError.Foreground = new SolidColorBrush(Colors.Green);
+
+                ClearAllInput();
+            }
+            else
+            {
+                labelError.Content = "Invalid input!";
+                labelError.Foreground = new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        public void Cancel(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
--- GUI/View/EditSubjectWindow.xaml.cs
+++ GUI/View/EditSubjectWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CLI.DAO;
+using CLI.Model;
 using GUI.DTO;
 using System;
 using System.Collections.Generic;
@@ -24,9 +25,71 @@ namespace GUI
         private HeadDAO _headDAO;
         private SubjectDTO _subjectDTO;
 
+        private Brush _defaultBrushBorder;
+
         public EditSubjectWindow(HeadDAO headDAO, SubjectDTO subjectOld)
         {
             InitializeComponent();
+            _headDAO = headDAO;
+            labelError.Content = string.Empty;
+
+            _defaultBrushBorder = textBoxName.BorderBrush.Clone();
+
+            _subjectDTO = new SubjectDTO(subjectOld);
+            DataContext = _subjectDTO;
+        }
+
+        private void ApplyEdit(object sender, RoutedEventArgs e)
+        {
+            if (InputCheck())
+            {
+                Subject subj = _subjectDTO.ToSubject();
+
+                _headDAO.daoSubject.UpdateObject(subj);
+
+                Close();
+            }
+            else
+            {
+                labelError.Content = "Invalid input!";
+                labelError.Foreground = new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        private bool InputCheck()
+        {
+            bool validInput = true;
+
+            foreach (var grid in stackPanel.Children.OfType<Grid>())
+            {
+                foreach (var control in grid.Children)
+                {
+                    if (control is TextBox)
+                    {
+                        TextBox textBox = (TextBox)control;
+                        if (textBox.Text == string.Empty)
+                        {
+                            textBox.BorderBrush = Brushes.Red;
+                            textBox.BorderThickness = new Thickness(2);
+                            validInput = false;
+                        }
+                        else
+                        {
+                            textBox.BorderBrush = _defaultBrushBorder;
+                            textBox.BorderThickness = new Thickness(1);
+                        }
+                    }
+                }
+            }
+
+            return validInput;
+        }
+
+
+
+        public void Cancel(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
